091c89f7defe69821fe21932cc70258f827cb8ed
Ragnar <raxsix@gmail.com>
2016-06-18 18:43:08 +0300

    building up controller

--- CheckSumCal/src/checksumcal/CheckSumCal.java
+++ CheckSumCal/src/checksumcal/CheckSumCal.java
@@ -5,10 +5,6 @@
  */
 package checksumcal;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,177 +17,14 @@ import javafx.stage.Stage;
  */
 public class CheckSumCal extends Application {
 
-    private static final int MAX_FILE_SIZE = 32768;
-    private static final String TEMP_FILE_NAME = "temp.bin";
-    private static final String RESULT_FILE_NAME = "result.bin";
-
-    private int mOriginalChecksum;
-    private int mTunedChecksum;
-    private int mCorrectionValue;
-
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
 
         Scene scene = new Scene(root);
 
-        // stage.setScene(scene);
-        // stage.show();
-        mOriginalChecksum = calculateChecksum("test.bin");
-
-        mTunedChecksum = calculateChecksum("test2.bin");
-
-        mCorrectionValue = calculateCorrectionValue(checkSumDivision(mOriginalChecksum, mTunedChecksum));
-
-        System.out.println("OriginalChecksum: " + mOriginalChecksum);
-        System.out.println("TunedChecksum: " + mTunedChecksum);
-
-        System.out.println("Division: " + checkSumDivision(mOriginalChecksum, mTunedChecksum));
-        System.out.println("Division in Hex: " + Integer.toHexString(checkSumDivision(mOriginalChecksum, mTunedChecksum)));
-
-        System.out.println("Correction: " + calculateCorrectionValue(checkSumDivision(mOriginalChecksum, mTunedChecksum)));
-
-        correctChecksum("test2.bin", mCorrectionValue, mOriginalChecksum);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
-
-    private static void correctChecksum(String filePath, int correctionValue, int originalChecksum) {
-
-        File f;
-        int intex = 0;
-        int count = 0;
-        int placeToWrite = 0;
-
-        try {
-            f = new File(filePath);
-            int[] array = new int[(int) f.length()];
-            int readByte;
-            try (FileInputStream fis = new FileInputStream(f)) {
-
-                while (fis.available() > 0) {
-
-                    for (int i = 0; i < 16; i++) {
-
-                        readByte = fis.read();
-                        array[intex] = readByte;
-                        intex++;
-                    }
-                }
-            }
-
-            for (int i = 0; i < array.length; i++) {
-
-                if (i % 16 == 0 && array[i] == 255 && array[i + 1] == 255 && array[i + 2] == 255) {
-
-                    for (int j = 0; j < correctionValue; j++) {
-
-                        if (array[i + j] == 255) {
-
-                            count++;
-                        }
-                    }
-
-                    if (count == correctionValue) {
-
-                        System.out.println("right place is: " + i + "->" + (i + correctionValue));
-                        placeToWrite = i;
-                        break;
-                    }
-                    count = 0;
-                }
-            }
-
-            for (int i = placeToWrite; i < (placeToWrite + correctionValue); i++) {
-
-                array[i] = 0;
-            }
-
-            try (OutputStream os = new FileOutputStream(TEMP_FILE_NAME)) {
-                for (int c : array) {
-                    os.write(c);
-                }
-                os.close();
-            }
-
-            int tempChecksum = calculateChecksum(TEMP_FILE_NAME);
-
-            File tempFile = new File(TEMP_FILE_NAME);
-            tempFile.delete();
-
-            array[placeToWrite + correctionValue] = finalCorrection(checkSumDivision(tempChecksum, originalChecksum));
-
-            try (OutputStream os = new FileOutputStream(RESULT_FILE_NAME)) {
-                for (int c : array) {
-                    os.write(c);
-                }
-                os.close();
-            }
-
-            int resultChecksum = calculateChecksum(RESULT_FILE_NAME);
-
-            if (resultChecksum == originalChecksum) {
-                System.out.println("CONCRATULATIONS TUNED CHECKSUM == ORIGINAL");
-            }
-
-        } catch (Exception e1) {
-
-        }
-    }
-
-    private static int finalCorrection(int divisionResult) {
-
-        return 255 - divisionResult;
-    }
-
-    private static int calculateCorrectionValue(int divisionResult) {
-
-        return divisionResult / 255;
-    }
-
-    public static int checkSumDivision(int original, int tuned) {
-
-        return Math.abs(tuned - original);
-    }
-
-    public final static int calculateChecksum(String filePath) {
-
-        File f;
-        long count = 0;
-        int devisionResult;
-        int multiplyResult;
-        int result = 0;
-        int intex = 0;
-
-        try {
-            f = new File(filePath);
-            int readByte;
-            try (FileInputStream fis = new FileInputStream(f)) {
-                while (fis.available() > 0) {
-                    char[] line = new char[16];
-                    for (int i = 0; i < 16; i++) {
-
-                        readByte = fis.read();
-
-                        count += readByte;
-
-//                        String paddingZero = (readByte < 16) ? "0" : "";
-//                        System.out.print(paddingZero + Integer.toHexString(readByte) + " ");
-//                        line[i] = (readByte >= 33 && readByte <= 126) ? (char) readByte : '.';
-                    }
-
-                    //System.out.println(new String(line));
-                }
-
-                devisionResult = (int) count / 65536;
-                multiplyResult = devisionResult * 65536;
-                result = (int) (count - multiplyResult);
-
-            }
-
-        } catch (Exception e1) {
-
-        }
-
-        return result;
-    }
-
 }
--- CheckSumCal/src/checksumcal/FXMLDocumentController.java
+++ CheckSumCal/src/checksumcal/FXMLDocumentController.java
@@ -5,31 +5,261 @@
  */
 package checksumcal;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Ragnar
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    private static final int MAX_FILE_SIZE = 32768;
+    private static final String TEMP_FILE_NAME = "temp.bin";
+    private static final String RESULT_FILE_NAME = "result.bin";
+
+    private int mOriginalChecksum;
+    private int mTunedChecksum;
+    private int mCorrectionValue;
+    private File mOriginalFile;
+    private File mTunedFile;
+
     @FXML
-    private Label label;
-    
+    private Button originalButton;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button tunedButton;
+
+    @FXML
+    private Button newChecksumButton;
+
+    @FXML
+    private Label originalLabel;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    private Label tunedLabel;
+
+    @FXML
+    private Label infoLabel;
+
+    @FXML
+    private Parent root;
+
+    @FXML
+    private void handleOriginalButtonAction(ActionEvent event) {
+
+        FileChooser fileChooser = new FileChooser();
+        Stage stage = (Stage) root.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+
+            if (event.getSource().equals(originalButton)) {
+                originalLabel.textProperty().set(file.getName());
+                mOriginalFile = file;
+            } else {
+                tunedLabel.textProperty().set(file.getName());
+                mTunedFile = file;
+            }
+        }
+
+        // If both files are selected only then we can hit the calculate button
+        if (mOriginalFile != null && mTunedFile != null) {
+            newChecksumButton.setDisable(false);
+        }
     }
-    
+
+    @FXML
+    private void handleCalculatedButtonAction(ActionEvent event) {
+
+        mOriginalChecksum = calculateChecksum(mOriginalFile.getAbsolutePath(), false);
+        originalLabel.setText("Checksum: " + Integer.toHexString(mOriginalChecksum).toUpperCase());
+
+        mTunedChecksum = calculateChecksum(mTunedFile.getAbsolutePath(), false);
+        tunedLabel.setText("Checksum: " + Integer.toHexString(mTunedChecksum).toUpperCase());
+
+        if (mOriginalChecksum < mTunedChecksum) {
+
+            mCorrectionValue = calculateCorrectionValue(checkSumDivision(mOriginalChecksum, mTunedChecksum));
+            correctChecksum(mTunedFile.getAbsolutePath(), mCorrectionValue, mOriginalChecksum);
+        } else {
+            infoLabel.setTextFill(Color.web("#bf0101"));
+            infoLabel.setText("ERROR");
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        newChecksumButton.setDisable(true);
+
+    }
+
+    public final static int calculateChecksum(String filePath, boolean withPrint) {
+
+        File f;
+        long count = 0;
+        int devisionResult;
+        int multiplyResult;
+        int result = 0;
+        int intex = 0;
+
+        try {
+            f = new File(filePath);
+            int readByte;
+            try (FileInputStream fis = new FileInputStream(f)) {
+                while (fis.available() > 0) {
+                    char[] line = new char[16];
+                    for (int i = 0; i < 16; i++) {
+
+                        readByte = fis.read();
+
+                        count += readByte;
+
+                        if (withPrint) {
+                            String paddingZero = (readByte < 16) ? "0" : "";
+                            System.out.print(paddingZero + Integer.toHexString(readByte) + " ");
+                            line[i] = (readByte >= 33 && readByte <= 126) ? (char) readByte : '.';
+                        }
+                    }
+
+                    if (withPrint) {
+                        System.out.println(new String(line));
+                    }
+
+                }
+
+                devisionResult = (int) count / 65536;
+                multiplyResult = devisionResult * 65536;
+                result = (int) (count - multiplyResult);
+
+            }
+
+        } catch (Exception e1) {
+
+        }
+        return result;
+    }
+
+    private void correctChecksum(String filePath, int correctionValue, int originalChecksum) {
+
+        File f;
+        int intex = 0;
+        int count = 0;
+        int placeToWrite = 0;
+
+        try {
+            f = new File(filePath);
+            int[] array = new int[(int) f.length()];
+            int readByte;
+            try (FileInputStream fis = new FileInputStream(f)) {
+
+                while (fis.available() > 0) {
+
+                    for (int i = 0; i < 16; i++) {
+
+                        readByte = fis.read();
+                        array[intex] = readByte;
+                        intex++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.length; i++) {
+
+                if (i % 16 == 0) {
+
+                    for (int j = 0; j < correctionValue + 32; j++) {
+
+                        if (array[i + j] == 255) {
+
+                            count++;
+                        }
+                    }
+
+                    if (count == (correctionValue + 32)) {
+
+                        System.out.println("right place is: " + i + "->" + (i + correctionValue));
+                        placeToWrite = i;
+                        break;
+                    }
+                    count = 0;
+                }
+            }
+
+            for (int i = placeToWrite; i < (placeToWrite + correctionValue); i++) {
+
+                array[i] = 0;
+            }
+
+            try (OutputStream os = new FileOutputStream(TEMP_FILE_NAME)) {
+                for (int c : array) {
+                    os.write(c);
+                }
+                os.close();
+            }
+
+            int tempChecksum = calculateChecksum(TEMP_FILE_NAME, false);
+
+            File tempFile = new File(TEMP_FILE_NAME);
+            tempFile.delete();
+
+            array[placeToWrite + correctionValue] = finalCorrection(checkSumDivision(tempChecksum, originalChecksum));
+
+            try (OutputStream os = new FileOutputStream(RESULT_FILE_NAME)) {
+                for (int c : array) {
+                    os.write(c);
+                }
+                os.close();
+            }
+            int resultChecksum = calculateChecksum(RESULT_FILE_NAME, true);
+
+            resultLabel.setText("result.bin Checksum is: " + Integer.toHexString(resultChecksum).toUpperCase());
+
+            if (resultChecksum == originalChecksum) {
+                System.out.println("VICTORY! TUNED CHECKSUM == ORIGINAL");
+                infoLabel.setTextFill(Color.web("#00a108"));
+                infoLabel.setText("SUCCESS!");
+            } else {
+                infoLabel.setTextFill(Color.web("#bf0101"));
+                infoLabel.setText("SOMETHING WENT WRONG");
+            }
+            System.out.println("changes made: " + placeToWrite + "->" + (placeToWrite + correctionValue));
+
+        } catch (Exception e1) {
+
+        }
+    }
+
+    private static int finalCorrection(int divisionResult) {
+
+        return 255 - divisionResult;
+    }
+
+    private static int calculateCorrectionValue(int divisionResult) {
+
+        return divisionResult / 255;
+    }
+
+    public static int checkSumDivision(int original, int tuned) {
+
+        return Math.abs(tuned - original);
+    }
+
 }
